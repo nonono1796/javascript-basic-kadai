aaa85df20404208fa011c1fed02a6e85f88e3b16
nozawamitsuki <kaminagara13@gmail.com>
2023-08-19 12:30:30 +0900

    kadai_20の提出

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+// HTML要素をclassで取得し、定数に代入する。
+const btnElement=document.getElementById('btn');
+const textElement=document.getElementById('text');
+// ボタンをクリックした時に表示を変える
+btnElement.addEventListener('click',()=>{
+    setTimeout(() => {
+        textElement.textContent='ボタンをクリックしました';
+    }, 2000);
+})
